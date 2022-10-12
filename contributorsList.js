34218f82aa9f4bb205f4a4f419f2f89d9773fe7e
Swaijit-Singh <115584497+Swaijit-Singh@users.noreply.github.com>
2022-10-12 12:18:39 +0530

    Create contributorsList.js

---
+++ contributorsList.js
@@ -0,0 +1,15 @@
+contributors = [
+  {
+    id: 1,
+    fullname: "Swaijit Singh",
+    username: "https://github.com/Swaijit-Singh",
+  },
+  
+  {
+    id: 2,
+    fullname: "Your Name",
+    username: "https://github.com/yourusername",
+  },
+
+
+];
