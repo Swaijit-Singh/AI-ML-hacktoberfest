9fc053e7c6215b157efb5c10f8ad432527d3124a
Swaijit-Singh <115584497+Swaijit-Singh@users.noreply.github.com>
2022-10-14 11:41:56 +0530

    Update contributorsList.js

--- contributorsList.js
+++ contributorsList.js
@@ -10,6 +10,12 @@ contributors = [
     fullname: "Manas Lohani",
     username: "https://github.com/manaslohani",
   },
+  
+  {
+    id: 3,
+    fullname: "Prasad Somvanshi",
+    username: "https://github.com/Prasad1121",
+  },
 
 
 ];
